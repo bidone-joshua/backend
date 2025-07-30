fbaf53c7e9b45b8d3ace09f1878bf780000492fa
Joshua Masangkay <joshua.masangkay@bidone.co.nz>
2025-07-30 18:12:39 +1200

    fix url

--- tests/e2e.test.js
+++ tests/e2e.test.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const axios_1 = __importDefault(require("axios"));
 describe('E2E: GET /hello from live server', () => {
     it('should return Hello, world!', async () => {
-        const res = await axios_1.default.get('http://localhost:3000/hello');
+        const res = await axios_1.default.get('http://134.33.248.254/hello'); // get using kubectl get svc
         expect(res.status).toBe(200);
         expect(res.data).toEqual({ message: 'Hello, world!' });
     });
--- tests/e2e.test.ts
+++ tests/e2e.test.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 describe('E2E: GET /hello from live server', () => {
   it('should return Hello, world!', async () => {
-    const res = await axios.get('http://localhost:3000/hello');
+    const res = await axios.get('http://134.33.248.254/hello'); // get using kubectl get svc
     expect(res.status).toBe(200);
     expect(res.data).toEqual({ message: 'Hello, world!' });
   });
