b5f9374bdee7091077758b191d736f8358712647
Joshua Masangkay <joshua.masangkay@bidone.co.nz>
2025-07-30 19:04:44 +1200

    Test

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors';
 
 const app = express();
+
+// Enable CORS
+app.use(cors()); // Allow all origins
 
 app.get('/hello', (req: Request, res: Response) => {
   res.json({ message: 'Hello, world!' });
